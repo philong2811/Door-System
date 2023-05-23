a22c5e55276d702b9af1b489db6bd4e2d7e2f920
philong2811 <dophilong2811@gmail.com>
2023-05-23 13:26:11 +0700

    main

--- rfid/rfid.ino
+++ rfid/rfid.ino
@@ -4,10 +4,11 @@
 #define RST_PIN 9
 #define SS_PIN 10
 #define RELAY 6
-#define ACCESS_DELAY 5000
-#define DENIED_DELAY 1000
 MFRC522 mfrc522(SS_PIN, RST_PIN);
- 
+
+static int access = 5000;
+static int denied = 1000;
+
 void setup() 
 {
   Serial.begin(9600);
@@ -15,7 +16,7 @@ void setup()
   mfrc522.PCD_Init();
   pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, LOW);
-  Serial.println("Put your card to the reader...");
+  Serial.println("Put your card");
   Serial.println();
 }
 void loop() 
@@ -31,7 +32,6 @@ void loop()
   
   Serial.print("UID tag :");
   String content= "";
-  byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
@@ -39,20 +39,17 @@ void loop()
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  Serial.println();
-  Serial.print("Message : ");
   content.toUpperCase();
   if (content.substring(1) == "9F 11 CB 28")
   {
-    Serial.println("Authorized access");
+    Serial.println("Access");
     Serial.println();
-    delay(500);
     digitalWrite(RELAY, HIGH);
-    delay(ACCESS_DELAY);
+    delay(access);
     digitalWrite(RELAY, LOW);
   }
  else   {
     Serial.println("Access denied");
-    delay(DENIED_DELAY);
+    delay(denied);
   }
 }
